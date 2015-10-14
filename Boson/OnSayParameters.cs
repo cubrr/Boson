4c5d382a629def8457dc392c6eb29be684c3bae3
cubrr <jay@thecuber.org>
2015-10-14 23:55:34 +0300

    Add license texts to files, separate OnSayParameters

---
+++ Boson/OnSayParameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using InfinityScript;
+
+namespace Boson
+{
+    internal struct OnSayParameters
+    {
+        public Entity Caller { get; private set; }
+
+        public BaseScript.ChatType ChatType { get; private set; }
+
+        public ICollection<string> Arguments { get; private set; }
+
+        public OnSayParameters(Entity caller, BaseScript.ChatType chatType, ICollection<string> args)
+            : this()
+        {
+            Caller = caller;
+            ChatType = chatType;
+            Arguments = args;
+        }
+    }
+}
