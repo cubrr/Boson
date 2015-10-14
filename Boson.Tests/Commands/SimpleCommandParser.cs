bb325ed2e324153a2ea2669f8973003acc357488
cubrr <jay@thecuber.org>
2015-10-15 02:24:41 +0300

    Scratch parsing testing with different prefixes

--- Boson.Tests/Commands/SimpleCommandParser.cs
+++ Boson.Tests/Commands/SimpleCommandParser.cs
@@ -68,99 +68,85 @@ namespace Boson.Tests.Commands
         [TestClass]
         public class TryParseMethod
         {
-            private const string ShortParserPrefix = "!";
-            private const string ShortParserDelimiter = " ";
-            private const string LongParserPrefix = "<!--";
-            private const string LongParserDelimiter = "..;..";
+            private const string CommandArgument1 = "cubrr";
+            private const string CommandArgument2 = "hax";
+            private const string ParserPrefix = "!";
+            private const string ParserDelimiter = " ";
             private readonly string[] _commands = new[]
             {
                 "s", "test", "debug", "superman", "kick", "ban", "ac130superdupercoolparty"
             };
 
-            private SimpleCommandParser _shortParser;
-            private SimpleCommandParser _longParser;
+            private SimpleCommandParser _parser;
             private string _outCommand;
             private IList<string> _outParams;
 
             [TestInitialize]
             public void TryParseInitialization()
             {
-                _shortParser = new SimpleCommandParser(ShortParserPrefix, ShortParserDelimiter);
-                _longParser = new SimpleCommandParser(LongParserPrefix, LongParserDelimiter);
+                _parser = new SimpleCommandParser(ParserPrefix, ParserDelimiter);
             }
 
             [TestMethod]
             public void NullMessage_ReturnsFalse()
             {
-                bool result = _shortParser.TryParse(null, out _outCommand, out _outParams);
+                bool result = _parser.TryParse(null, out _outCommand, out _outParams);
                 Assert.IsFalse(result);
             }
 
             [TestMethod]
             public void EmptyMessage_ReturnsFalse()
             {
-                bool result = _shortParser.TryParse("", out _outCommand, out _outParams);
+                bool result = _parser.TryParse("", out _outCommand, out _outParams);
                 Assert.IsFalse(result);
             }
 
             [TestMethod]
-            public void MessageShorterThanShortPrefix_ReturnsFalse()
+            public void MessageShorterThanPrefix_ReturnsFalse()
             {
-                bool result = _shortParser.TryParse(" ", out _outCommand, out _outParams);
+                bool result = _parser.TryParse(" ", out _outCommand, out _outParams);
                 Assert.IsFalse(result);
             }
 
             [TestMethod]
-            public void MessageShorterThanLongPrefix_ReturnsFalse()
+            public void PrefixedEmptyCommand_ReturnsFalse()
             {
-                bool result = _longParser.TryParse(" ", out _outCommand, out _outParams);
+                bool result = _parser.TryParse(ParserPrefix, out _outCommand, out _outParams);
                 Assert.IsFalse(result);
             }
 
             [TestMethod]
-            public void ShortPrefixedEmptyCommand_ReturnsFalse()
-            {
-                bool result = _shortParser.TryParse(ShortParserPrefix, out _outCommand, out _outParams);
-                Assert.IsFalse(result);
-            }
-
-            [TestMethod]
-            public void LongPrefixedEmptyCommand_ReturnsFalse()
-            {
-                bool result = _longParser.TryParse(LongParserPrefix, out _outCommand, out _outParams);
-                Assert.IsFalse(result);
-            }
-
-            [TestMethod]
-            public void ShortPrefixedCommandsWithoutArgs_ReturnsTrue()
+            public void CommandsWithoutArgs_ReturnTrue()
             {
                 foreach (string command in _commands)
                 {
-                    string prefixedCommand = ShortParserPrefix + command;
-                    bool result = _shortParser.TryParse(prefixedCommand, out _outCommand, out _outParams);
+                    string prefixedCommand = ParserPrefix + command;
+                    bool result = _parser.TryParse(prefixedCommand, out _outCommand, out _outParams);
                     Assert.IsTrue(result);
                 }
             }
 
             [TestMethod]
-            public void LongPrefixedCommandsWithoutArgs_ReturnsTrue()
+            public void CommandsWithoutArgs_GetParsed()
             {
                 foreach (string command in _commands)
                 {
-                    string prefixedCommand = LongParserPrefix + command;
-                    bool result = _longParser.TryParse(prefixedCommand, out _outCommand, out _outParams);
-                    Assert.IsTrue(result);
-                }
-            }
-
-            [TestMethod]
-            public void ShortPrefixedCommandsWithoutArgs_ParsesCommand()
-            {
-                foreach (string command in _commands)
-                {
-                    string prefixedCommand = ShortParserPrefix + command;
-                    bool result = _shortParser.TryParse(prefixedCommand, out _outCommand, out _outParams);
+                    string prefixedCommand = ParserPrefix + command;
+                    bool result = _parser.TryParse(prefixedCommand, out _outCommand, out _outParams);
                     Assert.AreEqual(command, _outCommand);
+                }
+            }
+
+            [TestMethod]
+            public void CommandsWithOneArgument_ReturnTrue()
+            {
+                foreach (string command in _commands)
+                {
+                    string message = String.Join(ParserDelimiter,
+                                                 ParserPrefix + command,
+                                                 CommandArgument1);
+                    bool result = _parser.TryParse(message, out _outCommand, out _outParams);
+                    Assert.IsTrue(result);
                 }
             }
         }
